78c48e23e1f1f05bc01afe6a8e6b7132d86beb1f
Sullmin <sullivan.geslot@epitech.eu>
2024-07-19 20:23:22 +0200

    - Clear IpcCoreService

--- app/src/main/java/com/example/android_ipc_grpc/ipc/IpcCoreService.kt
+++ app/src/main/java/com/example/android_ipc_grpc/ipc/IpcCoreService.kt
@@ -1,30 +1,11 @@
 package com.example.android_ipc_grpc.ipc
 
 import IpcCoreGrpcKt
-import android.util.Log
-import androidx.compose.runtime.currentCompositionLocalContext
-import androidx.compose.runtime.mutableStateOf
+import IpcCoreOuterClass
 import com.example.android_ipc_grpc.IpcApplication
 import com.example.android_ipc_grpc.db.schemas.Message
-import io.grpc.stub.StreamObserver
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asFlow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.emptyFlow
-import kotlinx.coroutines.flow.flowOf
-import kotlinx.coroutines.flow.forEach
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import java.util.LinkedList
-import java.util.Queue
-import java.util.stream.Stream
 
 class IpcCoreService : IpcCoreGrpcKt.IpcCoreCoroutineImplBase() {
     private val messageDao = IpcApplication.database.messageDao()
@@ -44,7 +25,6 @@ class IpcCoreService : IpcCoreGrpcKt.IpcCoreCoroutineImplBase() {
             IpcCoreOuterClass.SubscribeResponse.newBuilder()
                 .addAllMessages(
                     messages.map {
-                        Log.e("DEBUG", "subscribe message ${it.content}")
                         IpcCoreOuterClass.Message.newBuilder()
                             .setSource("")
                             .setMessage(it.content)
